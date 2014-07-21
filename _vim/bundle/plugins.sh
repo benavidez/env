58b0feed25b0708db39e240ddc55054904ddb17f
release <release@sntool.(none)>
2014-07-21 12:42:09 -0700

    adds plugins script to download plugins

---
+++ _vim/bundle/plugins.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jeffkreeftmeijer/vim-numbertoggle.git
+git clone https://github.com/tpope/vim-rails.git
+git clone https://github.com/tpope/vim-surround.git
+
